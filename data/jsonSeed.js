434ee858eb2e1041226ee2da67c011c444bb7716
Elizabeth Cannon <elizabeth230@gmail.com>
2019-03-02 22:02:14 -0800

    add file for generating nested json data for mongo

--- data/jsonSeed.js
+++ data/jsonSeed.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const faker = require('faker');
+const agents = require('./agents.json')
 
-generateAgents(10);
-// generateListings(10);
+// generateAgents(1000000);
+generateListings(10000000);
 
 function generateAgents(number) {
   let writableAgents = fs.createWriteStream('./data/agents.json', { 'flags': 'a' });
@@ -18,7 +19,7 @@ function generateAgents(number) {
       phone: faker.phone.phoneNumberFormat(1),
       premier: faker.random.arrayElement([true, false]),
       zip: faker.address.zipCode('#####'),
-      photo: faker.image.avatar()
+      photo: faker.image.avatar(),
     };
     if (i === 1) {
       data += '[';
@@ -31,25 +32,41 @@ function generateAgents(number) {
     }
     writableAgents.write(data);
     data = '';
-    if (i % 100 === 0) {
+    if (i % 100000 === 0) {
       console.log(`${i} agents created`);
     }
   }
 }
 
 function generateListings(number) {
-  let writeableListings = fs.createWriteStream('./data/listings.json', { 'flags': 'a' });
+  let writableListings = fs.createWriteStream('./data/listings.json', { 'flags': 'a' });
 
   let data = '';
   for (let i = 1; i <= number; i++) {
+    let randomAgent = Math.ceil(Math.pow(Math.random(), 2) * 1000000);
     let obj = {
       id: i,
       street: faker.address.streetAddress(),
       city: faker.address.city(),
       state: faker.address.stateAbbr(),
       zip: faker.address.zipCode('#####'),
+      agent: agents[randomAgent],
     };
+    if (i === 1) {
+      data += '[';
+    }
     data += JSON.stringify(obj);
+    if (i === number) {
+      data += ']';
+    } else {
+      data += ',';
+    }
+    if (i % 100000 === 0) {
+      writableListings.write(data);
+      data = '';
+    }
+    if (i % 1000000 === 0) {
+      console.log(`${i} listings created`);
+    }
   }
-  writeableListings.write(data);
-};
+}
