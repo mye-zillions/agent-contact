76c8f197e29819167d3e685b2c0d5a2b30aa0f3f
Elizabeth Cannon <elizabeth230@gmail.com>
2019-03-12 11:44:31 -0700

    Setup routes properly to use loader io

--- server/index.js
+++ server/index.js
@@ -9,13 +9,17 @@ const agents = require('./controllers/agents');
 const app = express();
 const PORT = process.env.PORT || 8082;
 
-app.use('/:houseId', express.static(path.resolve(__dirname, '../client/dist')));
+app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+app.get('/:houseId', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
 });
 
 app.post('/api/houses', houses.createNewHouse);
